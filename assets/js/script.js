ba572450808d647d4cfa657256aa303159dfd4ee
Francisco V. Diaz <fevarga1@uc.cl>
2022-08-29 08:49:04 -0400

    working in entrega 01

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,51 @@
-let respuesta = prompt("¡Bienvenido a la sumadora básica!\nIngresa 1 si quieres sumar. Cualquier otra cosa si quieres salir.")
+const textoBienvenida = "Bienvenido a tu galería de imágenes virtual."
+const textoInstrucciones = `
+    Escribe 1 para agregar imagenes.
+    Escribe 2 para saber cuantas imágenes has ingresado hasta el momento.
+    Escribe 3 para consultar las imágenes que has ingresado en esta sesión.
+    Escribe cualquier otra cosa para salir.`
+let imagenes = ""
+let cantidadImagenes = 0
 
-if (respuesta == "1") {
-    let cantidad_numeros = parseInt(prompt("¿Cuantos números quieres intentar sumar?"))
-    let resultado = 0
-    for (i = 0; i < cantidad_numeros; i++) {
-        resultado += parseInt(prompt("Ingresa el número " + (i + 1) + ":"))
+Main()
+
+function Main(){
+    alert(textoBienvenida)
+    while (true) {
+        let respuesta = parseInt(prompt(textoInstrucciones))
+        switch (respuesta) {
+            case 1:
+                AgregarImagen()
+                break;
+            case 2:
+                MostrarCantidadImagenes()
+                break;
+            case 3:
+                MostrarNombresImagenes()
+                break;
+            default:
+                alert("¡Hasta la próxima!")
+                return;
+        }
     }
-    alert("El resultado es: " + resultado)
 }
-else {
-    alert("¡Hasta la próxima!")
+
+function AgregarImagen() {
+    let repeticiones = parseInt(prompt("¿Cuántas imágenes quieres agregar?"))
+    for (i = 0; i < repeticiones; i++) {
+        let nombre = prompt(`Ingresa el nombre de la imagen ${i + 1}:`)
+        GrabarImagen(nombre)
+    }
 }
+
+function GrabarImagen(nombre) {
+    imagenes += nombre + " "
+    cantidadImagenes++
+}
+function MostrarCantidadImagenes() {
+    alert(`Has ingresado ${cantidadImagenes} imágenes`)
+}
+function MostrarNombresImagenes() {
+    alert(`Las imágenes que has ingresado son: ${imagenes}`)
+}
+
