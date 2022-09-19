cb8a4de6e8352c8ed37dfea6d2a4de726ce5da49
Francisco V. Diaz <fevarga1@uc.cl>
2022-09-19 11:36:49 -0300

    variables changed to english almost.  summary working but design frontend missing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,40 @@
-// OBJETO IMAGEN
-class Imagen {
-    constructor(nombre, tamanio) {
-        this.nombre = nombre;
-        this.tamanio = tamanio;
+// OBJECTS
+class Image {
+    constructor(name, size, dataUrl) {
+        this.name = name;
+        this.size = size;
+        this.dataUrl = dataUrl;
     }
 }
-//Objetos del DOM
-const subtitle = document.getElementById("subtitulo");
-const nImagenes = document.getElementById("n_imagenes");
-const cantidadInput = document.getElementById('cantidad');
+// DOM HTML OBJECTS
+const summaryTitle = document.getElementById("summaryTitle");
+const imagesSummary = document.getElementById("n_imagenes");
+const imagesAmount = document.getElementById('cantidad');
 const inputFiles = document.getElementById('inputfiles');
 const form = document.getElementById('formulario');
-const infoEspacio = document.getElementById('info-espacio');
+const spaceInfo = document.getElementById('info-espacio');
 const usedMemory = document.getElementById('memory-used')
 const totalMemory = document.getElementById('total-memory');
 const availableMemory = document.getElementById('memory-available');
 const totalImages = document.getElementById('total-images');
 
-// Variables Globales
-const espacioMaximo = 100 // el usuario puede subir maximo hasta 100mb a la plataforma.
-const maxFiles = 10; //El usuario puede ingresar maximo 10 imagenes
-let imagenes = localStorage.getItem('images');
-let usedSpace = 0; 
+// GLOBAL VARIABLES
+const maxSpace = 100 // USER MAX UPLOAD TO PLATFORM
+const maxFiles = 10; // MAX UPLOAD FILE INPUTS AT A TIME
+let userImages = localStorage.getItem('images');
+let usedSpace = 0;
 
-const reader = new FileReader();
-const preview = document.querySelector('#img');
-preview.src = localStorage.getItem('imagenPrueba');
 
 // MAIN
-imagenes = imagenes == null ? [] : JSON.parse(imagenes);
+userImages = userImages == null ? [] : JSON.parse(userImages);
 updateStats();
 
 
-// Eventos
-cantidadInput.oninput = () => {
-    cantidadInput.value = Math.max(0, Math.min(cantidadInput.value, maxFiles)); //Hace un clamp del valor ingresado min=0 y max=10
-    const veces = parseInt(cantidadInput.value);
-    inputFiles.innerHTML = ''; //Hace un clear a la cantidad de elementos previos
+// EVENTS
+imagesAmount.oninput = () => {
+    imagesAmount.value = Math.max(0, Math.min(imagesAmount.value, maxFiles)); //CLAMPS VALUE TO MIN=0 AND MAX=10
+    const veces = parseInt(imagesAmount.value);
+    inputFiles.innerHTML = ''; //CLEARS PREVIOUS HTML SUMMARY ELEMENTS IF ANY.
     for (i = 0; i < veces; i++) {
         const fileUpload = document.createElement('div');
         fileUpload.innerHTML = `<input class="uploadinput d-block mx-auto my-2" type="file" name="img" accept="image/*" value=Select>`
@@ -46,71 +44,70 @@ cantidadInput.oninput = () => {
 form.onsubmit = (e) => {
     e.preventDefault();
     const uploads = document.getElementsByClassName('uploadinput');
-    const userUploads = Array.from(uploads).filter(element => element.files.length > 0) // filtra los file inputs solo los que el usuario les cargó efectivamente una imagen.
+    const userUploads = Array.from(uploads).filter(element => element.files.length > 0) // REMOVES EMPTY UPLOAD INPUT FILES.
     createImages(userUploads);
     updateStats();
-    // mostrarResumen();
+    showSummary(userUploads);
 }
-reader.onload = () => {
-    preview.src = reader.result;
-    localStorage.setItem('imagenPrueba', reader.result);
-}
-// Funciones
+// FUNCTIONS
 function createImages(uploadList) {
     for (upload of uploadList) {
-        const nombre = upload.files[0].name;
-        const tamanio = upload.files[0].size / 1048576; //el tamaño se convierte de bytes a MB.
-        const imagen = new Imagen(nombre, tamanio);
-        // const test = URL.createObjectURL(upload.files[0]);
-        // console.log(test);
-        reader.readAsDataURL(upload.files[0]);
-        imagenes.push(imagen);
+        const file = upload.files[0];
+        const name = file.name;
+        const size = toMB(file.size); // SIZE CONVERTED TO MB.
+        const reader = new FileReader(); // JS OBJECT FOR STORAGE USER FILE AT BUFFER.
+        reader.readAsDataURL(file);
+        const image = new Image(name, size, reader.result);
+        userImages.push(image);
     }
 }
 
-// DOM Funciones
-function mostrarResumen() {
+// DOM FUNCTIONS
+function showSummary(userUploads) {
     clearInfo();
-    const text = document.createElement("h2");
-    text.innerText = "Resumen de tus acciones";
-    subtitle.append(text);
-    if (imagenes.length > 0) {
-        nImagenes.innerHTML = `<h4>Cantidad de imágenes:</h4>
-            <p>${imagenes.length}</p>`;
-        const nombreImagenes = imagenes.map(imagen => imagen.nombre);
-        const tituloListaNombres = document.createElement("h4");
-        tituloListaNombres.innerText = "Nombres de Imágenes ingresadas:"
-        nImagenes.append(tituloListaNombres)
-        for (nombre of nombreImagenes) {
-            const nombreImagen = document.createElement("p");
-            nombreImagen.innerText = `${nombre}`;
-            nImagenes.append(nombreImagen);
+    if (userUploads.length > 0) {
+        const title = document.createElement("h2");
+        title.innerText = "SUMMARY";
+        summaryTitle.append(title);
+        imagesSummary.innerHTML = `<h4>Number of images:</h4>
+            <p>${userUploads.length}</p>`;
+        const subtitle = document.createElement("h4");
+        subtitle.innerText = "Successfully uploaded images:"
+        imagesSummary.append(subtitle)
+        for (image of userUploads) {
+            const file = image.files[0];
+            const imageName = document.createElement("p");
+            imageName.innerText = `${file.name} - ${toMB(file.size).toFixed(2)}MB`;
+            imagesSummary.append(imageName);
         }
+        const totalSize = toMB(userUploads.reduce((accumulator, images) => accumulator + images.files[0].size, 0)); //CONVERTS TOTAL USER UPLOADED IMAGES BYTES TO MB. 
+        const uploadsSizeSummary = document.createElement("h5");
+        uploadsSizeSummary.innerText = `You've just spent ${totalSize.toFixed(2)} MB of ${maxSpace} MB total space.
+        ${(100 - (totalSize / maxSpace * 100)).toFixed(2)}% free space available.`
+        spaceInfo.append(uploadsSizeSummary);
     } else {
-        nImagenes.innerText = `No ingresaste ninguna imagen`;
+        imagesSummary.innerText = `You haven't chosen any images yet.`;
     }
-    const tamanio = imagenes.reduce((acumulador, imagen) => acumulador + imagen.tamanio, 0);
-    
-    const espacioUsado = document.createElement("h5");
-    espacioUsado.innerText = `Has utilizado ${tamanio.toFixed(2)} MB de los ${espacioMaximo} MB disponibles.
-    ${(100 - (tamanio / espacioMaximo * 100)).toFixed(2)}% restante disponible.`
-    infoEspacio.append(espacioUsado);
 }
 function clearInfo() {
-    subtitle.innerHTML = '';
-    nImagenes.innerHTML = '';
-    infoEspacio.innerHTML = '';
+    summaryTitle.innerHTML = '';
+    imagesSummary.innerHTML = '';
+    spaceInfo.innerHTML = '';
 }
-function updateStats(){
-    if (imagenes.length > 0){
-        usedSpace = imagenes.reduce((acumulador, imagen) => acumulador + imagen.tamanio, 0);
+function updateStats() {
+    if (userImages.length > 0) {
+        usedSpace = userImages.reduce((accumulator, images) => accumulator + images.size, 0);
     }
     else {
         usedSpace = 0;
     }
     usedMemory.innerText = `${usedSpace.toFixed(2)}MB`;
-    availableMemory.innerText = `${(espacioMaximo - usedSpace).toFixed(2)}MB`;
-    totalImages.innerText = imagenes.length;
-    totalMemory.innerText = `${espacioMaximo}MB`;
-    localStorage.setItem('images', JSON.stringify(imagenes));
+    availableMemory.innerText = `${(maxSpace - usedSpace).toFixed(2)}MB`;
+    totalImages.innerText = userImages.length;
+    totalMemory.innerText = `${maxSpace}MB`;
+    localStorage.setItem('images', JSON.stringify(userImages));
+}
+// TOOL FUNCTIONS
+function toMB(bytes) {
+    return bytes / 1048576;
 }
