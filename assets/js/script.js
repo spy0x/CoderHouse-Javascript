e0acdca25e5f8079ab4ae69358d74cf5d15f71b0
Francisco V. Diaz <fevarga1@uc.cl>
2022-09-23 10:13:25 -0300

    removed swal test

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,12 +27,7 @@ let usedSpace = 0;
 // MAIN
 userImages = userImages == null ? [] : JSON.parse(userImages);
 updateStats();
-Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool'
-  })
+
 
 // EVENTS
 imagesAmount.oninput = () => {
