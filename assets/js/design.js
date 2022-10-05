18e0b5596df2923fc883f8d835b29edbbf429bc9
Francisco V. Diaz <fevarga1@uc.cl>
2022-10-05 10:14:44 -0300

    added aside in html with nasa picture of the day

--- assets/js/design.js
+++ assets/js/design.js
@@ -2,9 +2,24 @@ const uploadSection = document.getElementById("upload");
 const navBar = document.getElementById("navbar");
 
 setUploadSectionScrollOffset();
+setNasaPicture();
 
 onresize = setUploadSectionScrollOffset;
 
 function setUploadSectionScrollOffset() {
     uploadSection.style.scrollMarginTop = `${navBar.offsetHeight}px`;
+}
+
+// AJAX
+async function setNasaPicture()
+{
+    const nasaImage = document.getElementById("nasa_picture");
+    const nasa_data = await fetch('https://api.nasa.gov/planetary/apod?api_key=bJZsLNUVSmRUjmQutduKbILfloBZw61P8C488kxu');
+    const result = await nasa_data.json();
+    //INFO FOR DOM
+    const mediaType = result.media_type;
+    const url = result.url;
+    // const title = result.title;
+    nasaImage.src = url;
+    nasaImage.type = mediaType;
 }
