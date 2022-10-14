21d613e8202b20be910872838c9179cba89f8832
Francisco V. Diaz <fevarga1@uc.cl>
2022-10-13 22:15:31 -0300

    gallery working sorting.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -5,40 +5,38 @@ const navBar = document.getElementById('navbar');
 const header = document.getElementsByTagName('header');
 const gallerySection = document.getElementById('gallery');
 const btnRemove = document.getElementById('btn__remove');
-const selectOrder = document.getElementById('select_order');
+const selectOrder = document.getElementById('select__order');
 
 // GLOBAL VARIABLES
 const userImages = JSON.parse(localStorage.getItem('images')) || []; //LOADS IMAGES FROM LOCALSTORAGE. IF NULL, THEN RETURNS EMPTY ARRAY.
 let selectedCards = [];
 
 // MAIN
-btnRemove.style.display = 'none';
+selectOrder.value = localStorage.getItem('select_order') || '';
+sortUserImages();
+clearSelection();
 showPictures();
-const cards = document.getElementsByClassName('card');
-for (element of cards) {
-  pictureClickEvent(element);
-}
+saveUserImages();
 
 // EVENTS
-onresize = () => (header[0].style.marginTop = `${navBar.offsetHeight}px`);
-onload = () => (header[0].style.marginTop = `${navBar.offsetHeight}px`);
+onresize = () => {
+  header[0].style.marginTop = `${navBar.offsetHeight}px`;
+};
+onload = () => {
+  header[0].style.marginTop = `${navBar.offsetHeight}px`;
+};
 btnRemove.onclick = () => {
   removeSelectedImages();
-  localStorage.setItem('images', JSON.stringify(userImages));
+  clearSelection();
   showPictures();
+  saveUserImages();
 };
 selectOrder.onchange = () => {
-  switch (selectOrder.value) {
-    case "2":
-      //Newest first
-      userImages.sort((a, b) => DateTime.fromISO(b.dt).toMillis() - DateTime.fromISO(a.dt).toMillis());
-      break;
-    case "3":
-      //Oldest first
-      userImages.sort((a, b) => DateTime.fromISO(a.dt).toMillis() - DateTime.fromISO(b.dt).toMillis());
-      break;
-    }
-    showPictures();
+  localStorage.setItem('select_order', selectOrder.value);
+  sortUserImages();
+  clearSelection();
+  showPictures();
+  saveUserImages();
 };
 function pictureClickEvent(card) {
   card.onclick = () => {
@@ -50,12 +48,20 @@ function pictureClickEvent(card) {
       card.style.background = 'linear-gradient(0deg, rgba(200, 200, 200, 1) 0%, rgba(248, 251, 255, 1) 100%)';
     }
     btnRemove.style.display = selectedCards.length > 0 ? 'block' : 'none';
+    gallerySection.style.marginBottom = `${btnRemove.offsetHeight}px`;
   };
 }
 
 // FUNCTIONS
 function showPictures() {
   gallerySection.innerHTML = '';
+  if (userImages.length == 0) {
+    const message = document.createElement('div');
+    message.classList.add('text-center', "text-danger", "m-auto", "main__section__container", "p-5");
+    message.innerText = 'Your gallery is empty.';
+    gallerySection.append(message);
+    return;
+  }
   for (image of userImages) {
     const { name, dataUrl, dt } = image;
     const date = DateTime.fromISO(dt);
@@ -64,13 +70,17 @@ function showPictures() {
     imageCard.innerHTML = `<div class="card h-100">
             <img src="${dataUrl}" class="card-img-top card__img" alt="...">
             <div class="card-body">
-            <h5 class="card-title" id="name">${name}</h5>
+            <h5 class="card-title text-center fw-bold" id="name">${name}</h5>
             </div>
             <div class="card-footer text-muted text-end">
-            ${date.toFormat('dd-MM-yyyy, HH:mm')}
+            ${date.toFormat('dd-MM-yyyy, HH:mm:ss')}
             </div>
             </div>`;
     gallerySection.append(imageCard);
+  }
+  const cards = document.getElementsByClassName('card');
+  for (element of cards) {
+    pictureClickEvent(element);
   }
 }
 function removeSelectedImages() {
@@ -80,5 +90,27 @@ function removeSelectedImages() {
     if (index > -1) {
       userImages.splice(index, 1);
     }
+  }
+}
+function saveUserImages() {
+  localStorage.setItem('images', JSON.stringify(userImages));
+}
+function clearSelection() {
+  btnRemove.style.display = 'none';
+  selectedCards = [];
+}
+function sortUserImages() {
+  switch (selectOrder.value) {
+    case '1':
+      userImages.sort((a, b) => a.name.localeCompare(b.name));
+      break;
+    case '2':
+      //Newest first
+      userImages.sort((a, b) => DateTime.fromISO(b.dt).toMillis() - DateTime.fromISO(a.dt).toMillis());
+      break;
+    case '3':
+      //Oldest first
+      userImages.sort((a, b) => DateTime.fromISO(a.dt).toMillis() - DateTime.fromISO(b.dt).toMillis());
+      break;
   }
 }
